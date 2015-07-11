7f48d708f4af81d72e6ed44436800b330d14b94c
Lanzafame <adrianlanzafame92@gmail.com>
2015-07-11 16:32:33 +1000

    FIX: Wrong type in function param. UPDATE: .travis.yml

--- src/DSPHelp.jl
+++ src/DSPHelp.jl
@@ -1,11 +1,11 @@
 module DSPHelp
 export ev, od
 
-function ev(x::Array{Int64,2})
+function ev(x::Array{Float64,2})
   return (x + fliplr(x))/2
 end
 
-function od(x::Array{Int64,2})
+function od(x::Array{Float64,2})
   return (x - fliplr(x))/2
 end 
 # package code goes here
